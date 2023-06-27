c2da64df791441b1d5b153b19a0b9b5f4bd128c4
ZerohRaG3 <kyledickey02@gmail.com>
2023-06-27 09:12:02 -0600

    Update HomePage.js

--- react-web/src/pages/HomePage.js
+++ react-web/src/pages/HomePage.js
@@ -7,7 +7,7 @@ export default function HomePage() {
 
     <ChakraProvider theme={theme}>
       <Box h={'100vh'} >
-        <Spline aria-hidden={'true'} scene="https://prod.spline.design/h9lum21vjzEXnlHT/scene.splinecode" />
+        <Spline aria-hidden={'true'} scene="https://prod.spline.design/5myx-z1U5t7Ny2Oe/scene.splinecode" />
 
         {/* <Heading pos={'absolute'} top={0} >Object</Heading> */}
       </Box>
